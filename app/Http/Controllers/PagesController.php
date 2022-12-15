c41b0c9a44acd6845c5f60805a24b6ebf151a850
Michelqflo <mishellmendoza.959670@gmail.com>
2022-12-14 22:31:12 -0500

    Sitio sem15 Terminado con paginacion

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,7 +6,7 @@ use App\Models\Estudiante;
 
 class PagesController extends Controller
 {
-    //
+    // PORTADA BIENVENIDA
     public function fnIndex() {
         return view('welcome');
     }
@@ -16,14 +16,79 @@ class PagesController extends Controller
         return view('Estudiante.pagDetalle', compact('xDetAlumnos'));
     }
 
+    //ELIMINAR
+    public function fnEliminar($id) {
+        $deleteAlumno = Estudiante::findOrFail($id);  //Datos de BD
+        $deleteAlumno->delete();
+        return back()->with('msj', 'Se elimino con exito...');
+    }
+
+    //LECTURA DE DATOS
     public function fnLista() {
-        $xAlumnos = Estudiante::all();  //Datos de BD
+        //$xAlumnos = Estudiante::all();  //Datos de BD
+        $xAlumnos = Estudiante::paginate(4);
         return view('pagLista', compact('xAlumnos'));
     }
 
-    public function fnGaleria ($numero=0) {
-      //  return "foto codigo: ".$numero;
-      return view('pagGaleria', ['valor'=> $numero, 'otro'=>25]) ;
+    //ACTUALIZAR
+    public function fnEstActualizar($id){                   //Paso 1
+
+        $xActAlumnos = Estudiante::findOrFail($id);
+        return view('Estudiante.pagActualizar', compact('xActAlumnos'));
     }
 
+
+    public function fnUpdate(Request $request, $id){                   //Paso 1
+
+        $xUpdateAlumnos = Estudiante::findOrFail($id);
+
+        $xUpdateAlumnos -> codEst = $request -> codEst;
+        $xUpdateAlumnos -> nomEst = $request -> nomEst;
+        $xUpdateAlumnos -> apeEst = $request -> apeEst;
+        $xUpdateAlumnos -> fnaEst = $request -> fnaEst;
+        $xUpdateAlumnos -> turMat = $request -> turMat;
+        $xUpdateAlumnos -> semMat = $request -> semMat;
+        $xUpdateAlumnos -> estMat = $request -> estMat;
+
+        $xUpdateAlumnos -> save();          //Guardado en BD
+        
+        return back()->with('msj','Se actualizo con exito...');
+    }
+
+
+    // PARA REGISTRAR
+    public function fnRegistrar(Request $request){
+
+        //return $request->all();         //Prueba de "token" y datos recibidos
+
+        $request ->validate([
+            'codEst' => 'required',
+            'nomEst' => 'required',
+            'apeEst' => 'required',
+            'fnaEst' => 'required',
+            'turMat' => 'required',
+            'semMat' => 'required',
+            'estMat' => 'required'
+        ]);
+
+        $nuevoEstudiante = new Estudiante;
+        $nuevoEstudiante->codEst = $request->codEst;
+        $nuevoEstudiante->nomEst = $request->nomEst;
+        $nuevoEstudiante->apeEst = $request->apeEst;
+        $nuevoEstudiante->fnaEst = $request->fnaEst;
+        $nuevoEstudiante->turMat = $request->turMat;
+        $nuevoEstudiante->semMat = $request->semMat;
+        $nuevoEstudiante->estMat = $request->estMat;
+        
+        $nuevoEstudiante->save();
+        
+        //$xAlumnos = Estudiante1::all();                      //Datos de BD
+        //return view('pagLista', compact('xAlumnos'));        //Pasar a pagLista
+        return back()->with('msj','Se registro con éxito...'); //Regresar con msj
+    }
+
+    public function fnGaleria ($numero=0) {
+        return "foto codigo: ".$numero;
+        return view('pagGaleria', ['valor'=> $numero, 'otro'=>25]) ;
+         }
 }
